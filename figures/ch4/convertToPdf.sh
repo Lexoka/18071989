8cd2d27d0994b6c08832b5a787d2848d65bfeab3
Alexandre <lexoka@gmail.com>
2017-08-29 23:53:36 +0200

    ch4 going ballistic 1

--- figures/ch4/convertToPdf.sh
+++ figures/ch4/convertToPdf.sh
@@ -3,5 +3,12 @@
 for FILE in *.eps
 do
 	#convert $FILE "$(basename $FILE .eps).pdf"
-	epstopdf $FILE
+	epstopdf $FILE # Much better!
+done
+
+for FILE in *.svg
+do
+	#convert $FILE "$(basename $FILE .svg).pdf"
+	inkscape $FILE --export-pdf="$(basename $FILE .svg).pdf" # Much better!
+	# It seems that convert can handle any format but sucks with all of them.
 done
