cdb0d1a0955f0d80de64d0b4520752dce25c68a9
Alexandre <lexoka@gmail.com>
2017-09-11 19:32:42 +0200

    ch4 better af V time model

--- scripts_and_data/modelAfVTime.plot
+++ scripts_and_data/modelAfVTime.plot
@@ -4,10 +4,19 @@ set ylabel "Temps de sélection moyen normalisé/V"
 
 set output "timeVafFit.svg"
 
-f(x) = 0.002*x - 0.1
-sigma = 0.6
+#f(x) = 0.002*x - 0.1
+#sigma = 0.6
 
-g(x) = 0.3*(f(x)/sigma**2)*exp( (-f(x)*f(x))/(2*sigma**2) ) + 0.3
+#g(x) = 0.3*(f(x)/sigma**2)*exp( (-f(x)*f(x))/(2*sigma**2) ) + 0.3
+
+a = 1.41728564
+b = 0.67526069
+c = 0.01
+d = 0.31374369
+sigma = 2.31879762
+
+g(x) = a*((c*x-b)/sigma**2)*exp( (-(c*x-b)*(c*x-b))/(2*sigma**2) ) + d
+
 
 #fit g(x) "areaTimeFit.csv" u 1:2 via sigma # Helps, but not enough to justify deviating from the neat little 3
 # Try scipy.
--- scripts_and_data/optimizeFit.py
+++ scripts_and_data/optimizeFit.py
@@ -7,13 +7,42 @@ import scipy.optimize
 import scipy.stats
 import matplotlib.pyplot as plt
 
-def RayleighPdf(x, a, b, sigma, d):
+def RayleighPdf(x, a, b, d, sigma):
 	#sigma = 3
 	#b = 1
 	#a = 2.65
 	#d = 0.15
 	k = 2
 	res = a*((x-b)/sigma**k)*np.exp( (-(x-b)*(x-b))/(2*sigma**k) ) + d
+	return(res)
+
+
+def FixedRayleighPdf(x, a, b, c, d, sigma):
+	#sigma = 3
+	"""
+	a *= 0.1
+	b *= 0.1
+	c *= 0.001
+	d *= 0.1
+	"""
+	#a = 0.3
+	#b = 0.1
+	#c = 0.002
+	#d = 0.3
+	#sigma = 0.6
+	k = 2
+	res = a*((c*x-b)/sigma**k)*np.exp( (-(c*x-b)*(c*x-b))/(2*sigma**k) ) + d
+	return(res)
+
+def ManualRayleigh(x, a,b,d, sigma):
+	"""
+	a = 0.0141728564
+	b = 0.0067526069
+	d = 0.0031374369
+	sigma = 0.01231879762
+	"""
+	k = 2
+	res = a*((x*0.01-b)/sigma**k)*np.exp( (-(x*0.01-b)*(x*0.01-b))/(2*sigma**k) ) + d
 	return(res)
 
 def EasyTest(x, a, b):
@@ -28,9 +57,31 @@ def FitFunc(df):
 	print(scipy.stats.pearsonr(df.TimeDivV, RayleighPdf(df.AreaDivV, *popt)))
 
 	plt.plot(df.AreaDivV, df.TimeDivV, 'b+', label='data')
-	plt.plot(df.AreaDivV, RayleighPdf(df.AreaDivV, *popt), "r-", label="LinFit")
+	plt.plot(df.AreaDivV, RayleighPdf(df.AreaDivV, *popt), "r-", label="RayleighPdf")
 	plt.show()
 
+def ManualFuncAf(afDf):
+	popt, pcov = scipy.optimize.curve_fit(ManualRayleigh, afDf.ARF, afDf.TimeDivSpeed)
+	print(popt)
+
+	print("pearsonr:")
+	print(scipy.stats.pearsonr(afDf.TimeDivSpeed, ManualRayleigh(afDf.ARF, *popt)))
+
+	plt.plot(afDf.ARF, afDf.TimeDivSpeed, "b+", label="data")
+	plt.plot(afDf.ARF, ManualRayleigh(afDf.ARF, *popt), "r-", label="RayleighPdf")
+	plt.show()
+
+def FitFuncAf(afDf):
+	afDf["UseAsX"] = afDf.ARF * 0.01
+	popt, pcov = scipy.optimize.curve_fit(RayleighPdf, afDf.UseAsX, afDf.TimeDivSpeed)
+	print(popt)
+
+	print("pearsonr:")
+	print(scipy.stats.pearsonr(afDf.TimeDivSpeed, RayleighPdf(afDf.UseAsX, *popt)))
+
+	plt.plot(afDf.UseAsX, afDf.TimeDivSpeed, "b+", label="data")
+	plt.plot(afDf.UseAsX, RayleighPdf(afDf.UseAsX, *popt), "r-", label="RayleighPdf")
+	plt.show()
 
 
 
@@ -38,13 +89,24 @@ def main():
 	if len(sys.argv) >= 2:
 		filename = sys.argv[1]
 		df = pd.read_csv(filename, sep="\t")
-		print("Original:")
+		print("Original areaTimeFit:")
 		print(df.head())
-		FitFunc(df)
+	#	FitFunc(df)
+
+		if len(sys.argv) >= 3:
+			print("")
+			afFilename = sys.argv[2]
+			afDf = pd.read_csv(afFilename, sep="\t")
+			print("Original afTimeFit:")
+			print(afDf.head())
+			#FitFuncAf(afDf)
+			ManualFuncAf(afDf)
 		#print(df.TimeDivV)
 	else:
-		print("Usage: please provide the path to the linear approx file you wish to reshape, e.g.:")
+		print("Usage: please provide the path to at least one file, e.g.:")
 		print(sys.argv[0] + " areaTimeFit.csv")
+		print("Or...")
+		print(sys.argv[0] + " areaTimeFit.csv afTimeFit.csv")
 
 
 if __name__ == "__main__":
